88ee92c5cca9b798a841b04289ebd9c58b8062fa
winston142O <winston1420@hotmail.com>
2025-10-27 13:16:38 -0400

    Update allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
   },
   server: {
     host: true,
-    port: 5173
+    port: 5173,
+    allowedHosts: ['mottos-frontend-production.up.railway.app', 'localhost']
   }
 })
